444f5136126f354e706e48020e95ddf2557b0aee
Bruno César <102826415+Bruno-Ces4r@users.noreply.github.com>
2022-06-29 17:32:45 -0300

    Add files via upload

---
+++ about-me-php/admin/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./admin-css/navbar.css">
+    <link rel="stylesheet" href="./admin-css/login.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <title>Admin-Page</title>
+</head>
+<body class="corpo">
+    <div class="main-box">
+        <div class="conteudo-esquerdo">
+            <!-- Vai vim uma imagem aqui-->
+        </div>
+        
+        <div class="conteudo-direito">
+            <div class="circle-login">
+                <p><img src="../image/foto-bruno.jpg" alt="" class="image-login"></p> <!--Trocar essa imagem-->
+            </div>
+            <p>Welcome to the Login Page, only admin can login on this page!</p>
+            <p>Ocupando aqui para ver</p>
+        </div>
+    </div>
+</body>
+</html>
