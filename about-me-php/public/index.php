0dd9a13d7cd7bfd3f43f05722cb2cbfe1158ba96
Bruno César <102826415+Bruno-Ces4r@users.noreply.github.com>
2022-07-05 17:30:47 -0300

    Add files via upload

---
+++ about-me-php/public/index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/navbar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../favicon_io/favicon.ico" type="image/x-icon">
+    <title>About Me</title>
+</head>
+<body class="corpo" >
+    
+    <?php include_once("./componentes/navbar.php"); ?>
+
+    
+    <section class="main-content">
+        <div class="left-content">
+            <div class="info-content">
+                <h5>Welcome!</h5>
+                <h1>I'm Full Stack Developer Bruno C. Camargo</h1>
+                <p>This WebSite was created with the intencion of publicizing my portfolio, covering my professional history, resume and a way for people to get in touch with me.</p>
+                <a href="./history.php"><button type='button' class="btn btn-success button">Learn More</button></a>
+                <a href="./contact.php" style="margin-left:0.8em;"><button type='button' class="btn btn-secondary button">Contact</button></a>
+            </div>
+        </div>
+        <div class="right-content">
+            <!-- COLOCAR UMA IMAGEM SEM FUNDO AQUI -->
+            <img src="../image/foto-bruno.jpg" alt="history" class="img-history">
+        </div>
+    </section>
+
+
+    <footer class="development-by">
+        Development by BC Developer
+        <a href=""><img src="../image/instagram.png" alt="" title="Instagram" class="img-contact"></a>
+        <a href=""><img src="../image/linkedin.png" alt="" title="Linkedin" class="img-contact"></a>
+        <a href=""><img src="../image/email.png" alt="" title="E-mail" class="img-contact"></a>
+        <a href=""><img src="../image/github.png" alt="" title="GitHub" class="img-contact"></a>
+    </footer>
+        <!--Interessante Colocar um modo Escuro,caso o usuário queira usar, é possível fazer isso com JavaScript-->
+</body>
+</html>
---
+++ about-me-php/src/views/home.php
@@ -0,0 +1,88 @@
+<?php 
+if(!($_SESSION)){
+    session_start();
+}
+
+if(!($_SESSION['username'])){
+    header("location:./../login/login.php");
+}
+
+require_once("./../action/conexao.php");
+$db = conecta();
+
+$sql = "SELECT * FROM tb_contact";
+
+$query = $db->query( $sql );
+
+$registros = $query->fetchall();
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="./../admin-css/navbar.css">
+    <link rel="stylesheet" href="./../admin-css/home.css">
+    <link rel="shortcut icon" href="./../../favicon_io/favicon.ico" type="image/x-icon">
+    <title>Home</title>
+</head>
+<body class="corpo">
+    <?php include_once("./../admin-componentes/navbar.php") ?>
+    <h1 class="titulo">Contacts</h1>
+
+    <div class="container">
+        <table class="table table-bordered">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Oportunity</th>
+                        <th scope="col">Creation day</th>
+                        <th scope="col">Creation Hour</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <?php
+                    $id = 0;
+                    $name = 1;
+                    $email = 2;
+                    $oportunity = 3;
+                    $day = 4;
+                    $i = 0;
+                    foreach($registros as $dados){
+                        echo('
+                                <tr>
+                                    <td>'.$dados[$id].'</td>
+                                    <td>'.$dados[$name].'</td>
+                                    <td>'.$dados[$email].'</td>
+                                    <td>'.$dados[$oportunity].'</td>
+                                    <td>'.$dados[$day].'</td>
+                                </tr>                               
+                        ');
+                        $i++;
+                    };
+                    if($i === 0){
+                        echo('
+                        <div class="bad-contact">
+                            <p>There are no contacts in the system!</p>
+                        </div>
+                        ');
+                    }
+                    else{
+                        echo('
+                        <div class="happy-contact">
+                            <p>Total Contacts: '.$i.'</p>
+                        </div>
+                        ');
+                    };
+                    ?>
+                </tbody>
+        </table>
+    </div>
+</body>
+</html>
