098d274c915b0f054e53d4e02fccc396d5d0ef51
Bruno César <102826415+Bruno-Ces4r@users.noreply.github.com>
2022-07-05 21:54:32 -0300

    Add files via upload

---
+++ about-me-php/public/history.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/history.css">
+    <link rel="stylesheet" href="./css/navbar.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="shortcut icon" href="../favicon_io/favicon.ico" type="image/x-icon">
+    <title>History</title>
+</head>
+<body class="corpo">
+    
+    <?php include_once("./componentes/navbar.php"); ?>
+    
+    <div class="timeline">
+        <div class="box esquerdo">
+            <div class="conteudo">
+            <h2>2022</h2>
+            <p>I was hired to do an internship at Banco do Brasil Seguros, during my journey in the company,I have been developing real projects, improving my performance in teamwork,developing my personal side and many other skills.
+            Currently, I continue to attend college on the 4th semester, improving my old projects and creating new personal projects.</p>
+            </div>
+        </div>
+        <div class="box direito">
+            <div class="conteudo">
+            <h2>2021</h2>
+            <p>I passed the Wise Up Online selection process, at the company, I developed my commercial and personal side, participating in online classes that were taught by Flávio Augusto and his team. I was in the 3rd semester of college and developed several projects of courses that I participated.</p>
+            </div>
+        </div>
+        <div class="box esquerdo">
+            <div class="conteudo">
+            <h2>2020</h2>
+            <p>This year was very important in my life, because it was when I decided on my professional future, when I chose the area of technology. I found out about JavaScript on the Curso em Vídeo platform, where I developed a great passion for technology.</p>
+            </div>
+        </div>
+    </div>
+    <footer class="development-by">
+        Development by BC Developer
+        <a href="https://www.instagram.com/bruno.cesar1/" target="__blank"><img src="../image/instagram.png" alt="" title="Instagram" class="img-contact"></a>
+
+        <a href="https://www.linkedin.com/in/bruno-c%C3%A9sar-659045206/" target="__blank"><img src="../image/linkedin.png" alt="" title="Linkedin" class="img-contact"></a>
+
+        <a href="mailto:bruno.reis.profissional@gmail.com" target="__blank"><img src="../image/email.png" alt="" title="E-mail" class="img-contact"></a>
+
+        <a href="https://github.com/Bruno-Ces4r" target="__blank"><img src="../image/github.png" alt="" title="GitHub" class="img-contact"></a>
+    </footer>
+    
+    <!-- <section class="conteinerbox">
+        <div class="box-content">
+
+            <h2>My History</h2>
+            <p>Hello, I'm Bruno César 👋 </p>
+            <p></p>
+            <p>Aos 16 anos, conheci o mundo da programação, que serviu de porta de entrada para o tão sonhado mundo da tecnológia,ao decorrer dos anos,desenvolvi uma grande paixão pela tecnológia e decidi trilhar meu caminho nessa área.</p>
+            <p>Ao decorrer dos anos,fiz diversos cursos,aprendi principalmente sobre Programação, Ánalise de Dados, Marketing Digital e Auto Conhecimento.</p>
+        </div>
+    </section>
+
+    <footer class="development-by">
+        Development by BC Developer
+        <a href=""><img src="./image/instagram.png" alt="" title="Instagram" class="img-contact"></a>
+        <a href=""><img src="./image/linkedin.png" alt="" title="Linkedin" class="img-contact"></a>
+        <a href=""><img src="./image/email.png" alt="" title="E-mail" class="img-contact"></a>
+        <a href=""><img src="./image/github.png" alt="" title="GitHub" class="img-contact"></a>
+    </footer> -->
+</body>
+</html>
