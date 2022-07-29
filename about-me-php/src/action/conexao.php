7abeedf9e2e37e7e09f6c350bae21231c636dc1f
Bruno César <bruno.reis.profissional@gmail.com>
2022-07-29 13:44:27 -0300

    att git

--- about-me-php/src/action/conexao.php
+++ about-me-php/src/action/conexao.php
@@ -1,6 +1,6 @@
 <?php
     function conecta(){
-        $host = 'localhost:33067';
+        $host = 'localhost:3306';
         $user = 'root';
         $password = 'Deckcity147';
         $database = 'about_me';
