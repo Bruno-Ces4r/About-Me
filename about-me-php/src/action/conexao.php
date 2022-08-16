7f8fcd4458083b0efa7f3c957b948c1cb61c2faa
Bruno César <102826415+Bruno-Ces4r@users.noreply.github.com>
2022-08-16 16:52:05 -0300

    Update conexao.php

--- about-me-php/src/action/conexao.php
+++ about-me-php/src/action/conexao.php
@@ -2,8 +2,8 @@
     function conecta(){
         $host = 'localhost:3306';
         $user = 'root';
-        $password = 'Deckcity147';
-        $database = 'about_me';
+        $password = '?';
+        $database = '?';
         $link = new PDO("mysql:host=$host;dbname=$database;charset=utf8",$user,$password);
         return $link;
     };
